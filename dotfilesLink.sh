8b7b79705f5dc7aef650cdaeb43d06a3fcaeef7e
yyoshiki41 <yyoshiki41@gmail.com>
2014-08-15 18:30:25 +0900

    Add gitignore_global

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
-ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
-ln -sf ~/dotfiles/.gitconfig  ~/.gitconfig
+ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
+ln -sf ~/dotfiles/.bashrc ~/.bashrc
+
+# git
+ln -sf ~/dotfiles/.gitignore_global ~/.gitignore_global
+ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
